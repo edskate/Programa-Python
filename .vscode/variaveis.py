8ba69052b41ec6b317bf674b7742b09e2c04cbc6
Edilson gomes de lima <edskateicevida@gmail.com>
2023-12-14 18:11:42 -0300

    tipos primitivos

---
+++ .vscode/variaveis.py
@@ -0,0 +1,26 @@
+nome_usuario = 'edy'
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'edy', 47
+estado = True
+
+
+
+# Função type()
+# print(type(media)) # class 'int'
+# print(type(n2)) # class 'float'
+# print(type(nome)) # class 'string' 
+# print(type(estado)) # class 'bool' tipo logico
+# print(type(1+2j)) # class `complexo`
+
+# Function insintance()
+
+# a = 10
+# b = 'Sol'
+# print(isinstance(a, (int,float)))
+
+
+a = 40
+c = 3
+r = a * c
+print(r)
